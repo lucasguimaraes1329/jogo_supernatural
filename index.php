a4468e36d5b03920428902c4e16a94bac2cc71fc
deisemorgado01 <morgadodeise17@gmail.com>
2026-09-15 16:16:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scooby-doo</title>
+    <title>Document</title>
 </head>
 <body>
     
